88ee3167eb087df08d9230d5795eea4afa200898
SarkNeha <snehassarker2006@gmail.com>
2026-03-15 20:52:39 +0100

    contact details

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 class Patient:
-    def __init__(self, name, age, illness):
+    def __init__(self, name, age, illness, contact):
         self.name = name
         self.age = age
         self.illness = illness
+        self.contact = contact
         self.admission_time = datetime.now()
 
     def display(self):
